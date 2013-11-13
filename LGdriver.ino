9e659957874bdad19b46544aeb8b23d5d7cb49b5
mwolfes <wolfes@kph.uni-mainz.de>
2013-11-13 13:19:06 +0000

    initial commit for calibration/manual mode of muC: only one mode, which sends adc values to epics

--- LGdriver.ino
+++ LGdriver.ino
@@ -10,17 +10,9 @@
 
 // TIMING
 #define T_SAMPLE 300 // [ ms ]
-#define T_WAITMOTOR 2500 // [ ms ]    ( wait for stop and adc again )
-#define T_SMALLSTEP 200 // [ ms ]  ( In position wackeln... )
 
 // constants
 const float lsb_adc = 15.625 / 1000000; 
-// Coarse Hysteresis
-const float coardse_l = 0.0040;
-const float coardse_r = 0.0040;
-// Fine Hysteresis
-const float fine_l = 0.0003;
-const float fine_r = 0.0003;
 
 // register devices
 //LCD
@@ -34,15 +26,12 @@ Port mdriver(4); // JEEPORT no 4
 
 // Process variables
 float adc_f = 0;
-float setpoint_f;
 bool direction = false;
 bool power = false;
-String EpicsRecord = "";
-bool EpicsRecComplete = false;
 
 //STATES
-enum { LISTEN , MSTART , MGO ,MSTARTFINE, MGOFINE,  MEND , ERROR } state = LISTEN;
-enum { State, MStatus , PVADC , Setpoint } outmesg = PVADC;
+enum { SEND } state = SEND;
+enum { State, PVADC } outmesg = PVADC;
 
 //  utils
 void ReadADC(){
@@ -54,21 +43,6 @@ void ReadADC(){
 	adc_f = raw * lsb_adc;
 } 
 
-void convert_String(){
-	char temp[EpicsRecord.length() + 1];
-	EpicsRecord.toCharArray( temp, sizeof(temp));
-	setpoint_f = atof(temp);
-}
-
-void initLCDline(byte n, byte startpos = 0){
-	for ( byte i = startpos ; i < NUM_LCD_COLS ; ++i){
-		lcd.setCursor(i,n);
-		lcd.print(' ');
-	}
-	lcd.setCursor(startpos,n);
-}
-
-
 //communication
 void sendState(){
 
@@ -77,26 +51,8 @@ void sendState(){
 	case State:
 		lcd.setCursor(8,0);
 		switch (state){
-		case LISTEN:
-			lcd.print("LISTEN     ");
-			break;
-		case ERROR:
-			lcd.print("ERROR STATE");
-			break;
-		case MSTART:
-			lcd.print("SET DIR    ");
-			break;
-		case MSTARTFINE:
-			lcd.print("SET FINEDIR");
-			break;
-		case MGO:
-			lcd.print("MOVING     ");
-			break;
-		case MGOFINE:
-			lcd.print("MOVING SLOW");
-			break;
-		case MEND:
-			lcd.print("WAITING    ");
+		case SEND:
+			lcd.print("Sending Voltage:");
 			break;
 		default:
 			lcd.print("ERROR! UDST");
@@ -104,109 +60,18 @@ void sendState(){
 		}
 		break;
 
-	case Setpoint:
-		//initLCDline(2,8);
-		lcd.setCursor(8,2);
-		lcd.print(setpoint_f,4);
-		lcd.print("V");
-		lcd.print(" ");
-		break;
-
 	case PVADC:
-		initLCDline(1,8);
+		lcd.setCursor(8,1);
 		lcd.print(adc_f,4);
-		lcd.print("V");
+		lcd.print("V   ");
 		Serial.print("adcpos");
 		Serial.println(adc_f,4);
 		break;
-
-	case MStatus:
-		// initLCDline(3,8);
-		lcd.setCursor(8,3);
-		if ( power ){
-			Serial.print("0s");
-			Serial.println(1);
-			if ( direction ) {
-				lcd.print("forward");
-				Serial.print("1s");
-				Serial.println(1);
-			} else {
-				lcd.print("reverse");
-				Serial.print("1s");
-				Serial.println(-1);
-			}
-		} else {
-			Serial.print("0s");
-			Serial.println(0);
-			lcd.print("off    ");
-		}
+	default:
+		lcd.setCursor(8,1);
+		lcd.print("ERROR      ");
 		break;
 	}
-}
-void serialEvent(){
-	//while (Serial.available()){
-	while (!EpicsRecComplete){
-		char incoming = (char) Serial.read();
-		EpicsRecord += incoming;
-		EpicsRecComplete = ( incoming == '\n' );
-	}
-}
-
-// Prepare to MOVE
-void Init_Start(){
-	convert_String();
-	outmesg = Setpoint;
-	sendState();
-	EpicsRecord = "";
-	EpicsRecComplete = false;
-	state=MSTART;
-}
-
-// Motor - CONTROLS
-void PowerOn(){
-	power = true;
-	mdriver.digiWrite(HIGH);
-	outmesg = MStatus;
-	sendState();
-}
-void PowerOff(){
-	power = false;
-	mdriver.digiWrite(LOW);
-	outmesg = MStatus;
-	sendState();
-}
-void _set_to(bool dir){
-	if ( direction != dir){	
-		// switch off motor befor changing polarity
-		bool power_old = power;
-		PowerOff();
-		delay(500);
-		direction = dir;
-		if ( dir ){
-			mdriver.digiWrite2(HIGH);
-		} else {
-			mdriver.digiWrite2(LOW);
-		}
-		sendState();
-		delay(500);
-		if (power_old) PowerOn();
-	}
-}
-bool SetMotorDirection(){
-	if ( ( setpoint_f - coardse_l < adc_f )
-	  && ( adc_f < setpoint_f + coardse_r ) ){
-		return 0;
-	}
-	_set_to( adc_f < setpoint_f );
-	return 1;
-}
-bool SetMotorDirectionFine(){
-	if ( ( setpoint_f - fine_l < adc_f )
-	  && ( adc_f < setpoint_f + fine_r ) ){
-		return 0;
-	}
-	_set_to( adc_f < setpoint_f );
-	return 1;
 }
 
 //MAIN EVT-LOOPS:
@@ -216,7 +81,6 @@ void setup(void) {
 
 	//Init serial console
 	Serial.begin(57600);
-	EpicsRecord.reserve(34);
 
 	//Init adc
 	adc.begin(0x1C);
@@ -233,16 +97,9 @@ void setup(void) {
 	lcd.setCursor(0,1);
 	lcd.print("ADC:    ");
 	lcd.setCursor(0,2);
-	lcd.print("EPICS:  ");
+	lcd.print("For calibration, std");
 	lcd.setCursor(0,3);
-	lcd.print("Motor:  ");
-	
-	// init
-	ReadADC();
-	outmesg = PVADC;
-	sendState();
-	outmesg = MStatus;
-	sendState();
+	lcd.print("Mode:checkout master");
 }
 
 
@@ -250,104 +107,15 @@ void loop() {
 
 	switch(state){
 
-	case LISTEN:
-		// Say: Listening to EPICS
-		outmesg = State;
-		sendState();
-		// once msg is finshed, get setpoint and go to MSTART
-		if ( EpicsRecComplete ){
-			Init_Start();
-		}
-		break;
+	case SEND:
 
-	case MSTART:
-		// Say: Set Motor Polarity
-		outmesg = State;
-		sendState();
-		// Update ADC - PV
-		ReadADC();
-		outmesg = PVADC;
-		sendState();
-
-		// Determine Direction
-		if ( SetMotorDirection() ){
-			state = MGO;
-		} else {
-			state = LISTEN; // Already in position => LISTEN
-		}
-		break;
-		
-	case MGO:
 		outmesg = State;
 		sendState();
 		
-		PowerOn();
 		delay(T_SAMPLE);
 		ReadADC();
 		outmesg = PVADC;
 		sendState();
-
-		if ( ( setpoint_f - coardse_l < adc_f )
-	  	  && ( adc_f < setpoint_f + coardse_r ) ){
-			PowerOff();
-			delay(T_WAITMOTOR);
-			ReadADC();
-			outmesg = PVADC;
-			sendState();
-			state = MSTARTFINE;
-		}
-		break;
-
-	case MSTARTFINE:
-		outmesg = State;
-		sendState();
-
-		if ( SetMotorDirectionFine() ){
-			state = MGOFINE;
-		} else {
-			state = LISTEN;
-		}
-		break;
-
-	case MGOFINE:
-		outmesg = State;
-		sendState();
-
-		PowerOn();
-		delay(T_SMALLSTEP);
-		PowerOff();
-		delay(T_WAITMOTOR);
-		
-		ReadADC();
-		outmesg = PVADC;
-		sendState();
-
-		if ( ( setpoint_f - fine_l < adc_f )
-	  	  && ( adc_f < setpoint_f + fine_r ) ){
-			state = MEND;
-		} else {
-			state = MGOFINE;
-		}
-		break;
-
-	case MEND:
-		outmesg = State;
-		sendState();
-
-		PowerOff();
-		delay(T_WAITMOTOR);
-		ReadADC();
-		outmesg = PVADC;
-		sendState();
-
-		state = LISTEN;
-		break;
-
-	case ERROR:
-		outmesg = State;
-		sendState();
-		PowerOff();
-		state = ERROR;
 		break;
 	}
 
